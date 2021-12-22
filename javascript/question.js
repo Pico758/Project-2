991d606f27b25d64fdc491828601430213121737
Pico758 <pkudlur@vibrenthealth.com>
2021-12-22 18:55:47 +0530

    Pico758: quiz operation

---
+++ javascript/question.js
@@ -0,0 +1,83 @@
+var questions = [{
+    "question": " who invented computer mouse?",
+    "option1": "Douglas Engelbart",
+    "option2": "Brendan Eich",
+    "option3": " vint cerf",
+    "option4": "louis pouzin",
+    "answer": "1"
+},
+{
+    "question": " what is CSS",
+    "option1": " cascade style sheet",
+    "option2": " current security service",
+    "option3": " curresnt state security",
+    "option4": " cinema session scene",
+    "answer": "1"
+},
+{
+    "question": " what is ROM?",
+    "option1": " read only memory",
+    "option2": " random access memory",
+    "option3": " read operate memory",
+    "option4": " reab over memory",
+    "answer": "1"
+},
+{
+    "question": " exapn RAM?",
+    "option1": " random access memory",
+    "option2": " read at memory",
+    "option3": " read and memory",
+    "option4": " read memory",
+    "answer": "1"
+},
+{
+    "question": " who develop javascript?",
+    "option1": "Brendan Eich",
+    "option2": " sam engl",
+    "option3": " stark",
+    "option4": " oak",
+    "answer": "1"
+},
+{
+    "question": " indian capital city ?",
+    "option1": " delhi",
+    "option2": " bangalore",
+    "option3": " kolkata",
+    "option4": " mumbai",
+    "answer": " 1"
+},
+{
+    "question": " karnataka capital city?",
+    "option1": " bangalore",
+    "option2": " kolkata",
+    "option3": " delhi",
+    "option4": " mumbai",
+    "answer": " 1"
+},
+{
+    "question": " who established vijayanagar empire?",
+    "option1": " hakka bukka",
+    "option2": " chandragupta",
+    "option3": " shivaji",
+    "option4": " pushyamitra sunga",
+    "answer": " 1"
+},
+{
+    "question": " current prime minister of bharat?",
+    "option1": " rahul gandhi",
+    "option2": " narendra modi",
+    "option3": " manamohan singh",
+    "option4": " amith sha",
+    "answer": " 2"
+},
+{
+    "question": " our nation emblem?",
+    "option1": " ashoka chakra",
+    "option2": " peaock",
+    "option3": " lion",
+    "option4": " lotus",
+    "answer": "1"
+},
+
+
+]
